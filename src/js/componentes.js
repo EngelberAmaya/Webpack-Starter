93d288349eb0e294614d336824e20924ad3b7e09
EngelberAmaya <engelberamaya@gmail.com>
2023-04-18 14:32:12 -0400

    Copy Plugin

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,5 +1,5 @@
 import '../css/componentes.css';
-import webpacklogo from '../assets/img/webpack-sprite.png';
+// import webpacklogo from '../assets/img/webpack-sprite.png';
 
 export const saludar = (nombre) => {
     console.log('Creando etiqueta h1');
@@ -11,8 +11,8 @@ export const saludar = (nombre) => {
 }
 
 // Img
-console.log(webpacklogo);
-const img = document.createElement('img');
-img.src = webpacklogo;
-document.body.append(img);
+// console.log(webpacklogo);
+// const img = document.createElement('img');
+// img.src = webpacklogo;
+// document.body.append(img);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin    = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -55,6 +56,14 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: '[name].css',
             ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'src/assets/',
+                    to: 'assets/'
+                }
+            ]
         })
     ]
 }
